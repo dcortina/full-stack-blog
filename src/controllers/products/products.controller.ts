7463fe2237fa526a0c4ef4b018b50eadb326e328
dcortina <76714201+dcortina@users.noreply.github.com>
2023-09-21 08:52:03 -0400

    Update products.controller.ts

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -16,26 +16,30 @@ import { ProductsService } from 'src/services/products/products.service';
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productService: ProductsService) {}
-
+//ver todos los productos
   @Get()
   @HttpCode(200)
   getAllProducts(): Product[] {
     return this.productService.getAllProducts();
   }
+  //ver un producto determinado
   @Get(':id')
   getProduct(@Param('id') id: number) {
     return this.productService.getProduct(id);
   }
+  //insertar un nuevo producto
   @Post()
   @HttpCode(HttpStatus.NO_CONTENT)
   createProduct(@Body() producto) {
     this.productService.insertProduct(producto);
     return this.productService.getAllProducts();
   }
+  //actualizar un producto
   @Put(':id')
   updateProduct(@Param('id') id: number, @Body() body) {
     return this.productService.updateProduct(id, body);
   }
+  //borrar un producto
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   deleteProduct(@Param('id') id: number) {
